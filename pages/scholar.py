0c6e41a8119f7eaa2e24e33dccbe64a4e37bf9d1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-10-13 08:54:03 +0100

    hide Igor

--- pages/scholar.py
+++ pages/scholar.py
@@ -14,7 +14,8 @@ class BarGraphPage(Page):
         self.data = []
 
         # Matthew, Jorgen, Igor
-        for id in ["wxM0Gh8AAAAJ", "hfeXoYMAAAAJ", "rSVxxwsAAAAJ"]:
+        for id in ["wxM0Gh8AAAAJ", "hfeXoYMAAAAJ"]:  # , "rSVxxwsAAAAJ"]:
+            print(id)
             author = scholarly.search_author_id(id).fill()
             count = sum(int(paper.bib["cites"])
                         for paper in author.publications)
